f34a92e113a8c4f41905244d0c39bc724d53284f
AdeDom <pathiphonyw@gmail.com>
2020-12-04 17:25:21 +0700

    version 0.3.3 api multi player

--- src/com/adedom/teg/models/request/MultiRequest.kt
+++
@@ -1,7 +0,0 @@
-package com.adedom.teg.models.request
-
-data class MultiRequest(
-    val roomNo: String? = null,
-    val latitude: Double? = null,
-    val longitude: Double? = null
-)
--- src/com/adedom/teg/models/response/RoomResponse.kt
+++
@@ -1,7 +0,0 @@
-package com.adedom.teg.models.response
-
-data class RoomResponse(
-    var success: Boolean = false,
-    var message: String? = null,
-    var roomNo: String? = null,
-)
